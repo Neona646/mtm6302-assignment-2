da61b0b3d6ffcfcd5fa7d0a9d00885460c16c65e
Neona646 <79280901+Neona646@users.noreply.github.com>
2021-03-20 20:12:31 -0400

    Add files via upload
    I'm still having trouble with this assignment. But I didn't had time to make it interactive with javascript.

---
+++ script.js
@@ -0,0 +1,21 @@
+const gallary = document.getElementById('gallary')
+
+const $image = document.querySelector('img')
+
+const empty = document.querySelector
+
+
+$images.forEach(img =>{
+  img.addEventListener('click', () => {
+    img.dataset = innerHTML = `
+    <h2> Characters </h2>`
+  })
+});
+
+
+$image.addEventListener('mouseover' , function(e){
+  console.log($image.dataset.name)
+   
+  //  console.log(image.dataset.imageType) // Two dataset words into one. 
+  //  console.log(image.dataset.name) // one dataset word
+})
